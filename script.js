9e664e759358ea5130a64945ad9e6e05cdcdc81b
Husniddin-web <husniddinsalohiddin1974@gmail.com>
2022-09-27 20:48:33 +0500

    javaScript home work-1

---
+++ script.js
@@ -0,0 +1,60 @@
+'use strict'
+// m -1 
+//   let sm=+prompt("santimetir kriting metrga ");
+//   let result=sm/100;
+//   console.log(`${result} metrga teng`);
+
+
+
+
+//   m-2
+// let kg=+prompt('vaziningizmi tonada bilib oling')
+// let result=kg/1000
+// console.log(`${result} tonna vazinigiz bor ekan`);
+
+
+// m-3
+// let a=+prompt('musbat son kirting:');
+// let b=+prompt('musbat  va a dan kichik son kirting:');
+// let result=Math.floor(a/b);
+// console.log(result);
+
+
+// m-4
+// let number =+prompt('ikki xonali son kiriting:')
+// let onlik=Math.trunc(number/10);
+// let birlik=number%10;
+// console.log(`${onlik} onliklar xonasidagi son ${birlik}  birliklar xonasidagi son`);
+
+
+// m -5
+// let number =+prompt('ikki xonali son kiriting:')
+// let onlik=Math.trunc(number/10);
+// let birlik=number%10;
+// console.log(`${onlik+birlik}  ga teng`);
+
+
+// m-6
+// let number =+prompt('ikki xonali son kiriting:')
+// let onlik=Math.trunc(number/10);
+// let birlik=number%10;
+// console.log(`${onlik*birlik}  ga teng`);
+
+
+
+// m-7
+// let number=+prompt('999 katta son kirting:')
+// let result=(number%1000)/100;
+// console.log(result);
+
+// m-8 
+
+// let sekund=+prompt('kun boshidan nechhi skeund otdi:');
+// let minut=Math.floor(sekund/60);
+// let soat=Math.floor(sekund/3600);
+// console.log(`${soat} soat ${minut}  minut otdi`);
+
+
+
+
+
